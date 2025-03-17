5d5ecdb97aeff23ab3eb356572ee3f5a47ee260e
AdamLipman <108160569+AdamLipman@users.noreply.github.com>
2025-03-16 21:12:52 -0400

    Update zero_coupon_bonds.py

--- zero_coupon_bonds.py
+++ zero_coupon_bonds.py
@@ -11,6 +11,7 @@ class ZeroCouponBonds:
     def calculate_price(self):
         return self.present_value(self.principal, self.maturity)
 
+
 if __name__ == '__main__':
     bond = ZeroCouponBonds(1000,2,4)
     print("Price of the bond in dollars: %.2f" % bond.calculate_price())
